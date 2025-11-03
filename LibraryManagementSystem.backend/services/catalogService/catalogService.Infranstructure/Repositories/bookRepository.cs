e3f15f0d2ffc640a0a11b388ed0d6a9f882d5bdd
Koliya03 <senarathnakoliya@gmail.com>
2025-11-03 14:14:38 +0530

    Updated the Infranstructure of catalog mricroservice

---
+++ LibraryManagementSystem.backend/services/catalogService/catalogService.Infranstructure/Repositories/bookRepository.cs
@@ -0,0 +1,43 @@
+﻿using Application.Interfaces;
+using Domain.Book.Entities;
+using Marten;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infranstructure.Repositories
+{
+    public class bookRepository:IBookRepository
+    {
+        private readonly IDocumentSession _session;
+
+        public bookRepository(IDocumentSession session)
+        {
+            _session = session;
+        }
+
+        public async Task AddBookAsync(Book book)
+        {
+           _session.Store(book);
+            await _session.SaveChangesAsync();
+        }
+
+        public async Task<List<Book>> GetAllAsync()
+        {
+            return (await _session.Query<Book>().ToListAsync()).ToList();
+        }
+
+        public async Task<Book?> GetByIdAsync(Guid id)
+        {
+            return await _session.LoadAsync<Book>(id);
+        }
+
+        public async Task UpdateBookAsync(Book book)
+        {
+            _session.Store(book);
+            await _session.SaveChangesAsync();
+        }
+    }
+}
